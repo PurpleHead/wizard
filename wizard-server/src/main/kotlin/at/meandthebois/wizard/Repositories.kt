97dbe7d1f70a0379a4e5a08628589251a7c14919
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2024-07-19 18:09:40 +0200

    Add Game and Player Repositories

---
+++ wizard-server/src/main/kotlin/at/meandthebois/wizard/Repositories.kt
@@ -0,0 +1,7 @@
+import org.springframework.data.repository.CrudRepository
+
+interface PlayerRepository : CrudRepository<Player, Long> {
+    fun findByName(name: String): Player?
+}
+
+interface GameRepository : CrudRepository<Game, Long>
