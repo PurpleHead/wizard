f6fe0aa5c756970fd6a26c54cfb6113f5b8f57fd
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2024-08-07 20:41:39 +0200

    Move controllers to /api/*

--- wizard-server/src/main/kotlin/at/meandthebois/wizard/web/Controllers.kt
+++ wizard-server/src/main/kotlin/at/meandthebois/wizard/web/Controllers.kt
@@ -6,23 +6,23 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/games")
+@RequestMapping("/api/games")
 class GameController(var gameService: GameService) {
     @PostMapping("/create")
     fun createGame(@RequestBody game: GameDTORequest): GameDTOResponse {
         return gameService.createGame(game)
     }
 
-    @GetMapping
+    @GetMapping("/list")
     fun getGames(): Iterable<GameDTOResponse> {
         return gameService.getGames()
     }
 }
 
 @RestController
-@RequestMapping("/players")
+@RequestMapping("/api/players")
 class PlayerController(var playerService: PlayerService) {
-    @GetMapping
+    @GetMapping("/list")
     fun getPlayers(): Iterable<PlayerDTOResponse> {
         return playerService.getPlayers()
     }
